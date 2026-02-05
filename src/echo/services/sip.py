98c76a0fe479b4f01129c9b5df44098a551d2e1f
adrijh <ajimenezh.94@gmail.com>
2026-02-05 09:50:49 +0100

    Use correct room_id

--- src/echo/services/sip.py
+++ src/echo/services/sip.py
@@ -77,7 +77,7 @@ class SipService:
 
         return None
 
-    async def create_room_for_call(self, event: events.StartSessionRequest, ai_number: str) -> str:
+    async def create_room_for_call(self, event: events.StartSessionRequest, ai_number: str) -> api.Room:
         room_name = f"room-{event.room_id}"
 
         await self.lk_api.room.create_room(
@@ -91,21 +91,22 @@ class SipService:
             "created_at": event.timestamp.isoformat()
         })
 
-        await self.lk_api.room.update_room_metadata(
+        return await self.lk_api.room.update_room_metadata(
             api.UpdateRoomMetadataRequest(room=room_name, metadata=meta_payload)
         )
 
-        return room_name
 
-    async def initiate_sip_call(self, room_name: str, event: events.StartSessionRequest, ai_number: str) -> None:
-        log.info(f"Initiating call to  {event.participant_name} - {event.phone_number} from {ai_number} in room {room_name}")
+    async def initiate_sip_call(self, room: api.Room, event: events.StartSessionRequest, ai_number: str) -> None:
+        log.info(
+            f"Initiating call to  {event.participant_name} - {event.phone_number} from {ai_number} in room {room.name}"
+        )
 
         try:
             sip_request = CreateSIPParticipantRequest(
                 sip_trunk_id=cfg.SIP_TRUNK_ID,
                 sip_number=ai_number,
                 sip_call_to=event.phone_number,
-                room_name=room_name,
+                room_name=room.name,
                 participant_identity=f"sip-{event.opportunity_id}",
                 participant_name=event.participant_name,
                 krisp_enabled=True,
@@ -115,7 +116,7 @@ class SipService:
             await self.lk_api.sip.create_sip_participant(sip_request)
             log.info(f"Call answered. Participant {event.participant_name} number: {event.phone_number}")
 
-            await self._start_recording(room_name, event.room_id)
+            await self._start_recording(room)
 
         except TwirpError as e:
             if ("Busy Here" in str(e) or
@@ -123,19 +124,19 @@ class SipService:
                 (e.metadata and e.metadata.get("sip_status_code") == "486")):
                 log.warning(
                     f"User {event.participant_name} busy or unavailable ({event.phone_number}). "
-                    f"Code: 486/Busy. Room {room_name} will be closed."
+                    f"Code: 486/Busy. Room {room.name} will be closed."
                 )
-                await self.cleanup_room(room_name)
+                await self.cleanup_room(room.name)
                 return
 
-            log.error(f"Twirp Error in call {room_name}: {e}", exc_info=True)
-            await self.cleanup_room(room_name)
+            log.error(f"Twirp Error in call {room.name}: {e}", exc_info=True)
+            await self.cleanup_room(room.name)
             raise e
 
         except Exception as e:
-            log.error(f"Unexpected Error in call {room_name}: {e}", exc_info=True)
+            log.error(f"Unexpected Error in call {room.name}: {e}", exc_info=True)
             try:
-                await self.cleanup_room(room_name)
+                await self.cleanup_room(room.name)
             except Exception as delete_err:
                 log.error(f"Error deleting room after call failure: {delete_err}")
             raise e
@@ -147,23 +148,23 @@ class SipService:
             log.error(f"Error deleting room {room_name}: {e}")
 
     @classmethod
-    async def run_background_call(cls, room_name: str, event: events.StartSessionRequest, ai_number: str) -> None:
+    async def run_background_call(cls, room: api.Room, event: events.StartSessionRequest, ai_number: str) -> None:
         service = cls()
         try:
-            await service.initiate_sip_call(room_name, event, ai_number)
+            await service.initiate_sip_call(room, event, ai_number)
         except Exception as e:
-            log.error(f"Error in background task for {room_name}: {e}")
+            log.error(f"Error in background task for {room.name}: {e}")
         finally:
             await service.close()
 
-    async def _start_recording(self, room_name: str, room_id: str) -> None:
+    async def _start_recording(self, room: api.Room) -> None:
         egress_request = RoomCompositeEgressRequest(
-            room_name=room_name,
+            room_name=room.name,
             audio_only=True,
             audio_mixing="DEFAULT_MIXING",
             file_outputs=[
                 EncodedFileOutput(
-                    filepath=(f"recordings/{room_id}/recording.ogg"),
+                    filepath=(f"recordings/{room.sid}/recording.ogg"),
                     azure=AzureBlobUpload(
                         account_name=cfg.AZURE_ACCOUNT_NAME,
                         account_key=cfg.AZURE_ACCOUNT_KEY,
