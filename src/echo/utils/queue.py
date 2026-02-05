43d39e868651d59967855b9f3577f7fb2c01b8df
adrijh <ajimenezh.94@gmail.com>
2026-02-05 18:45:54 +0100

    add queue

---
+++ src/echo/utils/queue.py
@@ -0,0 +1,73 @@
+import os
+from collections.abc import Awaitable, Callable
+from typing import Any, Protocol, Self
+
+import aio_pika
+from aio_pika.abc import (
+    AbstractChannel,
+    AbstractQueue,
+    AbstractRobustConnection,
+)
+
+import echo.events.v1 as events
+from echo.logger import configure_logger
+
+log = configure_logger(__name__)
+
+
+class Queue(Protocol):
+    async def start(self, callback: Callable[[Any], Awaitable[Any]]) -> None: ...
+    async def stop(self) -> None: ...
+
+
+class RabbitQueue:
+    def __init__(
+        self,
+        conn: AbstractRobustConnection,
+        channel: AbstractChannel,
+        queue: AbstractQueue,
+    ) -> None:
+        self.conn = conn
+        self.channel = channel
+        self.queue = queue
+
+    @classmethod
+    async def build(cls) -> Self:
+        conn = await RabbitQueue._get_queue_connection()
+        channel = await conn.channel()
+        await channel.set_qos(prefetch_count=1)
+
+        queue = await channel.declare_queue(os.environ["RABBITMQ_CHANNEL"], durable=False)
+        return cls(
+            conn=conn,
+            channel=channel,
+            queue=queue,
+        )
+
+    async def stop(self) -> None:
+        await self.channel.close()
+        await self.conn.close()
+
+    async def start(self, callback: Callable[[Any], Awaitable[Any]]) -> None:
+        await self.queue.consume(callback)
+
+    @staticmethod
+    async def _get_queue_connection() -> AbstractRobustConnection:
+        return await aio_pika.connect_robust(
+            host=os.environ["RABBITMQ_HOST"],
+            port=int(os.environ["RABBITMQ_PORT"]),
+            login=os.environ["RABBITMQ_USER"],
+            password=os.environ["RABBITMQ_PASSWORD"],
+        )
+
+    async def send_event(self, event: events.SessionEvent) -> None:
+        message = aio_pika.Message(
+            body=event.model_dump_json().encode(),
+            delivery_mode=aio_pika.DeliveryMode.PERSISTENT,
+            content_type="application/json",
+        )
+
+        await self.channel.default_exchange.publish(
+            message,
+            routing_key=self.queue.name,
+        )
