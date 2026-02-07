da18b4f2f87d529641f9f73b8b119dc658aa6814
Adrián Jiménez Hernández <ajimenezh.94@gmail.com>
2026-02-07 22:42:36 +0100

    add sas option to blob

--- src/echo/utils/storage.py
+++ src/echo/utils/storage.py
@@ -7,12 +7,20 @@ from echo.logger import configure_logger
 log = configure_logger(__name__)
 
 
-async def get_blob_content(blob_url: str) -> bytes | None:
-    async with BlobClient.from_blob_url(blob_url, credential=os.environ["AZURE_ACCOUNT_KEY"]) as client:
-        try:
+async def get_blob_content(blob_url: str, sas: bool = False) -> bytes | None:
+    try:
+        if sas:
+            client = BlobClient.from_blob_url(blob_url)
+        else:
+            client = BlobClient.from_blob_url(
+                blob_url,
+                credential=os.environ["AZURE_ACCOUNT_KEY"],
+            )
+
+        async with client:
             stream = await client.download_blob()
-            content = await stream.readall()
-            return content
-        except Exception as e:
-            log.error(f"Could not download blob with url '{blob_url}': {e}")
-            return None
+            return await stream.readall()
+
+    except Exception:
+        log.exception(f"Could not download blob with url '{blob_url}'")
+        return None
